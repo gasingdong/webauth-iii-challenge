0ed6a14d8a7b805371748e4d8127a92bec7f3add
Brandon Dong <gasingdong@gmail.com>
2019-09-18 13:27:58 -0700

    add register endpoint

--- api/server.ts
+++ api/server.ts
@@ -6,6 +6,7 @@ import express, {
 } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import bcryptjs from 'bcryptjs';
 import Users from '../users/user-model';
 
 const server = express();
@@ -14,10 +15,56 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+const validateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const { body } = req;
+
+  if (!body) {
+    res.status(400).json({ error: 'No body provided.' });
+    return;
+  }
+
+  const { username, password, department } = body;
+
+  if (username && password && department) {
+    try {
+      const existingUser = await Users.findBy({ username });
+
+      if (!existingUser) {
+        next();
+      } else {
+        res.status(400).json({ error: 'Username is already taken.' });
+      }
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    res
+      .status(400)
+      .json({ error: 'Users need a username, password, and deparment.' });
+  }
+};
+
 server.get('/api/users', async (req, res, next) => {
   try {
     const users = await Users.find();
     res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+server.post('/api/register', validateUser, async (req, res, next) => {
+  const user = req.body;
+  const hash = bcryptjs.hashSync(user.password);
+  user.password = hash;
+
+  try {
+    const saved = await Users.add(user);
+    res.status(201).json(saved);
   } catch (err) {
     next(err);
   }
--- users/user-model.ts
+++ users/user-model.ts
@@ -2,8 +2,31 @@ import { QueryBuilder } from 'knex';
 import db from '../database/db-config';
 import { User } from '../types';
 
+interface UserFilter {
+  username?: string;
+  department?: string;
+}
+
 const find = (): QueryBuilder<{}, User[]> => {
-  return db('users');
+  return db('users').select('id', 'username', 'department');
 };
 
-export default { find };
+const findBy = (filter: UserFilter): QueryBuilder<{}, User> => {
+  return db('users')
+    .where(filter)
+    .first<User>();
+};
+
+const findById = (id: number): QueryBuilder<{}, User> => {
+  return db('users')
+    .where({ id })
+    .first<User>();
+};
+
+const add = async (user: User): Promise<User> => {
+  const ids = await db('users').insert(user, 'id');
+  const [id] = ids;
+  return findById(id);
+};
+
+export default { find, findBy, findById, add };
